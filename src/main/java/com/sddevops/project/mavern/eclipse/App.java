0974e8f747e2af9293b1808ebfa8d615abd28bb3
5093616m <wmng920i@gmail.com>
2024-01-30 03:45:50 +0800

    Initial Commit, Wei Meng, 30 Jan 2024

---
+++ src/main/java/com/sddevops/project/mavern/eclipse/App.java
@@ -0,0 +1,13 @@
+package com.sddevops.project.mavern.eclipse;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
