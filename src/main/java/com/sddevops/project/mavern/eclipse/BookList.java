150ac4a1816ca315886176f95378e77f7623cb3a
5093616m <wmng920i@gmail.com>
2024-01-30 04:37:46 +0800

    ESLBMWA-18, Added System.out.println("Hello World!"), AK, 30 Jan 2024

---
+++ src/main/java/com/sddevops/project/mavern/eclipse/BookList.java
@@ -0,0 +1,11 @@
+package com.sddevops.project.mavern.eclipse;
+
+public class BookList {
+	  static void myMethod() {
+	    System.out.println("Hello World!");
+	  }
+
+	  public static void main(String[] args) {
+	    myMethod();
+	  }
+	}
