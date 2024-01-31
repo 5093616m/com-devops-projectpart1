ae441fb8f98e2b4e68f2c3b2c47a0c6624e5f68d
5093616m <wmng920i@gmail.com>
2024-02-01 04:22:49 +0800

    ESLBMWA-18, Changes ("Hello World") to ("Hello EVERYONE"), AK, 1 Feb 2024

--- src/main/java/com/sddevops/project/mavern/eclipse/BookList.java
+++ src/main/java/com/sddevops/project/mavern/eclipse/BookList.java
@@ -2,7 +2,7 @@ package com.sddevops.project.mavern.eclipse;
 
 public class BookList {
 	  static void myMethod() {
-	    System.out.println("Hello World!");
+	    System.out.println("Hello EVERYONE!");
 	  }
 
 	  public static void main(String[] args) {
