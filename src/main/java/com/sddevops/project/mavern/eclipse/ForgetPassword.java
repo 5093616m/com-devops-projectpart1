a334cc0d8f1ce0f64c5e1fb8b67996b847d30f36
5093616m <wmng920i@gmail.com>
2024-03-01 01:13:44 +0800

    added new settings files

---
+++ src/main/java/com/sddevops/project/mavern/eclipse/ForgetPassword.java
@@ -0,0 +1,18 @@
+package com.sddevops.project.mavern.eclipse;
+
+import java.util.ArrayList;
+
+public class ForgetPassword extends App {
+	public static void main(String[] args) { 
+	    ArrayList<String> numbers = new ArrayList<String>();
+	    numbers.add("One");
+	    numbers.add("Two");
+	    numbers.add("Three");
+	    numbers.add("Four");
+	    numbers.add("Five");
+	    numbers.add("Six");
+	    numbers.add("Seven");
+	    number.add("Nine");
+	    System.out.println(numbers);
+	  } 
+}
